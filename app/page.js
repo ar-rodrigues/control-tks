c99c0ab77f11dbc479e2c10cd960ad1709e46f12
ARodrigues <alissonrodrigues.st@gmail.com>
2024-08-02 13:23:14 -0600

    Configured Chakra UI

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+'use client'
+import { Link } from '@chakra-ui/next-js'
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <h1>Hello World</h1>
+      <Link href='/about' color='blue.400' _hover={{ color: 'blue.500' }}>
+      Hello World!
+    </Link>
     </main>
   );
 }
