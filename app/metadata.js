ac21ce9efc897eec7eed5a546452eaa1ec0f5274
ARodrigues <alissonrodrigues.st@gmail.com>
2024-08-02 11:15:51 -0600

    Initial Commit

---
+++ app/metadata.js
@@ -0,0 +1,45 @@
+// metadata.js
+
+const APP_NAME = "Control TKS";
+const APP_DEFAULT_TITLE = "Ctrl TKS";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "App for internal control of TKS activities";
+
+export const metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport = {
+  themeColor: "#FFFFFF",
+};
