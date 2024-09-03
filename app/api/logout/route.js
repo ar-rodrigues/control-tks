00b73e8a3af25e8c282666dd63d41633c74ab638
ARodrigues <alissonrodrigues.st@gmail.com>
2024-09-03 16:26:00 -0600

    logout fixted

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -5,5 +5,8 @@ export async function POST() {
   const supabase = createClient();
   await supabase.auth.signOut();
 
-  return NextResponse.redirect(new URL('/login', process.env.EMAIL_CONFIRM_URL).toString());
+  const url = new URL(request.url);
+  const baseUrl = `${url.protocol}//${url.host}`;
+
+  return NextResponse.redirect(new URL('/login', baseUrl).toString());
 }
