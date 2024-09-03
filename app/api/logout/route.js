ef1666e5ab5317f3535428b150a4078b968b4b36
ARodrigues <alissonrodrigues.st@gmail.com>
2024-09-03 16:37:17 -0600

    error on logout fixted

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -1,12 +1,18 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '../../utils/supabase/server';
 
-export async function POST() {
+export async function POST(request) {
   const supabase = createClient();
-  await supabase.auth.signOut();
 
-  const url = new URL(request.url);
-  const baseUrl = `${url.protocol}//${url.host}`;
+  try {
+    await supabase.auth.signOut();
 
-  return NextResponse.redirect(new URL('/login', baseUrl).toString());
+    const url = new URL(request.url);
+    const baseUrl = `${url.protocol}/${url.host}`;
+
+    return NextResponse.redirect(new URL('/login', baseUrl).toString());
+  } catch (error) {
+    console.error('Error during logout:', error);
+    // You can return an error response here if needed
+  }
 }
